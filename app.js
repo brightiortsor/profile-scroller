f5561f7d239552e1d794b181284462721bd9fc8d
Bright Iortsor <iortsor@gmail.com>
2022-08-25 04:42:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -95,8 +95,8 @@ function nextProfile(){
     `; 
  const displayNew = document.getElementById('imageDisplay');
  displayNew.innerHTML = `<img src="${currentProfile.image}"> `;
-        displayNew.style.width = "100%"
-        displayNew.style.height = "auto";
+        displayNew.style.paddingRight = "50px"
+       
     } else {
         window.location.reload();
     }
